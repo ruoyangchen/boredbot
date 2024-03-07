981dcac33bc0eaca5d3348f1dff15c645ab3a973
ruoyangchen <135175852+ruoyangchen@users.noreply.github.com>
2024-03-06 18:26:24 -0800

    update bored API link

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getActivityIdea() {
-    fetch("https://apis.scrimba.com/bored/api/activity")
+    fetch("http://www.boredapi.com/api/activity/")
         .then(res => res.json())
         .then(data => {
             document.getElementById("idea").textContent = data.activity
